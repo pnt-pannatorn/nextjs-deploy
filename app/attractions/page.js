7884a2ef7754b7265abde38baef549b078744d58
Pannatorn Siriswangporn <pannatornpts@gmail.com>
2025-03-31 10:20:23 +0700

    dsads

--- app/attractions/page.js
+++ app/attractions/page.js
@@ -24,7 +24,7 @@ export default async function page() {
   const data = await getData();
   return (
     <div>
-      <Typography variant="h5">Attractions</Typography>
+      <Typography variant="h5">Attractions123</Typography>
       <a href="/attractions/create">
         <Button variant="contained" sx={{ my: 2 }}>
           Create
